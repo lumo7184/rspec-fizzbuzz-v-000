4fc0841dc4e82a1175ff73aec8555251ed6a218b
lucas montoni <lmontoni@outlook.com>
2018-08-07 17:55:50 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,8 +1,8 @@
 require_relative './spec_helper.rb'
 # We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
-
-describe "fizzbuzz" do
-  it 'returns "Fizz" when the number is divisible by 3' do
+fizz= x
+def fizzbuzz 
+  if (fizz % 3 == 0)z" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
 
     expect(fizz_3).to eq("Fizz")
